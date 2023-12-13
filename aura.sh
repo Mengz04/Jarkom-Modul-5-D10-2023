e43ef175d1fb7093689ca4177878e6f52f12f0af
Muhammad Rafi Insan Fillah <rafifillah@gmail.com>
2023-12-13 19:02:08 +0700

    fix: prog 1-2

--- aura.sh
+++ aura.sh
@@ -1,9 +1,3 @@
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE -s 192.196.0.0/16
-
-apt-get update
-
-apt install isc-dhcp-relay -y
-
 echo '
 SERVERS="192.196.14.142"
 INTERFACES="eth1 eth2"
@@ -11,6 +5,12 @@ OPTIONS=""
 ' > /etc/default/isc-dhcp-relay
 
 echo 'net.ipv4.ip_forward=1' >  /etc/sysctl.conf
+
+sysctl -p
+
+apt-get update
+
+apt install isc-dhcp-relay -y
 
 service isc-dhcp-relay start
 
@@ -25,3 +25,42 @@ route add -net 192.196.14.144 netmask 255.255.255.252 gw 192.196.14.130 #A7 REF 
 #KANAN
 route add -net 192.196.8.0 netmask 255.255.252.0 gw 192.196.14.150 #A9 REF HEI A8
 route add -net 192.196.0.0 netmask 255.255.248.0 gw 192.196.14.150 #A10 REF HEI A8
+
+ip_address=$(ip addr show eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+
+# Display the assigned IP address
+echo "Assigned IP address for eth0: $ip_address"
+
+# Flush existing rules and set the default policies to DROP
+# iptables -F
+# iptables -P INPUT DROP
+# iptables -P FORWARD DROP
+# iptables -P OUTPUT ACCEPT
+
+# iptables -A FORWARD -p udp --dport 53 -j ACCEPT
+
+# iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+
+# iptables -A FORWARD -s 192.196.14.130 -j ACCEPT
+# iptables -A FORWARD -s 192.196.14.150 -j ACCEPT
+
+# # Allow incoming traffic on port 8080 (TCP)
+# iptables -A INPUT -p tcp --dport 8080 -j ACCEPT
+# iptables rule with SNAT
+
+# Drop all incoming TCP traffic
+iptables -A INPUT -p tcp -j DROP
+# Drop all incoming UDP traffic
+iptables -A INPUT -p udp -j DROP
+
+# Drop all outgoing TCP traffic
+iptables -A OUTPUT -p tcp -j DROP
+# Drop all outgoing UDP traffic
+iptables -A OUTPUT -p udp -j DROP
+
+# Allow incoming TCP traffic on port 8080
+iptables -A INPUT -p tcp --dport 8080 -j ACCEPT
+# Allow outgoing TCP traffic on port 8080
+iptables -A OUTPUT -p tcp --sport 8080 -j ACCEPT
+
+iptables -t nat -A POSTROUTING -o eth0 -j SNAT --to-source $ip_address
--- frieren.sh
+++ frieren.sh
@@ -1,9 +1,5 @@
 echo 'nameserver 192.168.122.1' > /etc/resolv.conf
 
-apt-get update
-
-apt install isc-dhcp-relay -y
-
 echo '
 SERVERS="192.196.14.142"
 INTERFACES="eth0 eth1 eth2"
@@ -11,6 +7,10 @@ OPTIONS=""
 ' > /etc/default/isc-dhcp-relay
 
 echo 'net.ipv4.ip_forward=1' >  /etc/sysctl.conf
+
+apt-get update
+
+apt install isc-dhcp-relay -y
 
 service isc-dhcp-relay start
 
