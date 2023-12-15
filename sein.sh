85f695aea15ccb3de77505bf31ec79fdb24122f5
kenanargya <92387421+kenanargya@users.noreply.github.com>
2023-12-15 14:00:45 +0700

    Update sein.sh

--- sein.sh
+++ sein.sh
@@ -2,6 +2,10 @@ echo 'nameserver 192.168.122.1' > /etc/resolv.conf
 
 echo 'net.ipv4.ip_forward=1' >  /etc/sysctl.conf
 
+apt-get update
+
+apt-get install netcat -y
+
 # Allow SSH from a specific IP address during specific time window
 iptables -A INPUT -p tcp --dport 22 -s 192.196.8.2 -m time --weekdays Mon,Tue,Wed,Thu,Fri --timestart 08:00 --timestop 16:00 -j ACCEPT
 
